5b1ca3a80e14c6dabef56e8e9382d03246d9793e
Mario Wu <mariusz.wincior@gmail.com>
2026-09-14 17:26:58 +0200

    fix(analytics): move integration data out of analytics

--- frontend/src/routes/_layout/ledgers.$ledgerId.analytics.tsx
+++ frontend/src/routes/_layout/ledgers.$ledgerId.analytics.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router"
 
 import { AnalyticsDashboard } from "@/components/Analytics/AnalyticsDashboard"
-import { ComponentHistoryExplorer } from "@/components/Analytics/ComponentHistoryTable"
 
 export const Route = createFileRoute("/_layout/ledgers/$ledgerId/analytics")({
   component: LedgerAnalytics,
@@ -10,10 +9,5 @@ export const Route = createFileRoute("/_layout/ledgers/$ledgerId/analytics")({
 
 function LedgerAnalytics() {
   const { ledgerId } = Route.useParams()
-  return (
-    <div className="space-y-8">
-      <AnalyticsDashboard ledgerId={ledgerId} />
-      <ComponentHistoryExplorer ledgerId={ledgerId} />
-    </div>
-  )
+  return <AnalyticsDashboard ledgerId={ledgerId} />
 }
