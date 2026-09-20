683310517c6c693bf05a1b3cf1acd1f94fb2bb00
Mario Wu <mariusz.wincior@gmail.com>
2026-09-20 17:06:36 +0200

    fix(test): expose e2e proxy on IPv4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: process.env.VITE_API_PROXY_TARGET ? "0.0.0.0" : undefined,
     proxy: process.env.VITE_API_PROXY_TARGET
       ? {
           "/api": {
